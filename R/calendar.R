87ebabaa755ba540928d4e8c892b09452a61ba6c
Mitch <mitch.ohara-wild@monash.edu>
2020-08-25 13:03:01 +1000

    Calendar origin is now bool for identifying originless moments

--- R/calendar.R
+++ R/calendar.R
@@ -5,15 +5,15 @@
 #' A calendar describes how time changes over some vector. It is responsible
 #' for converting a vector of numbers into a point in time.
 #'
-#' @param origin The initial time point for the calendar
 #' @param granularity An origin-less identifier for the time increment of each value.
+#' @param origin Logical identifier for if the origin is meaningful.
 #'
 #' @return A calendar data frame.
 #'
 #' @export
-new_calendar <- function(origin, granularity){
+new_calendar <- function(granularity, origin = TRUE){
   vctrs::new_data_frame(
-    list(origin = origin, granularity = granularity),
+    list(granularity = granularity, origin = origin),
     class = "calendar"
   )
 }
