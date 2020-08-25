38c9c38ee58a1261a3290f7c45cc94215f8570d5
Mitch <mitch.ohara-wild@monash.edu>
2020-08-25 13:48:56 +1000

    Added attributes to time_units for timezones and other features

--- R/time_unit.R
+++ R/time_unit.R
@@ -6,16 +6,18 @@
 #' used to identify how different time units nest another.
 #'
 #' @param x The number of units (must be an integer of length 1).
+#' @param ... Additional attributes of the time unit (for example, timezones).
 #' @param class The class of the unit. Recommended class format is "tu_*", for
 #'   example, "tu_month".
 #'
 #' @return A time unit.
 #'
 #' @export
-new_time_unit <- function(x, class){
+new_time_unit <- function(x, ..., class){
   vec_assert(x, ptype = integer(), size = 1L)
   vctrs::new_vctr(
     x,
+    ...,
     class = c(class, "time_unit")
   )
 }
