74b8eadd62a30dc02dbca3838ab47e9884d394de
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2025-08-26 18:12:24 +1000

    Add contextless duration class stub

---
+++ R/duration.R
@@ -0,0 +1,20 @@
+#' Create a temporal durations from time units
+#'
+#' @description
+#' `r lifecycle::badge("experimental")`
+#' 
+#' Constructs a new duration object (`mt_duration`) from a set of mixtime time 
+#' units. This functionality is in active development and is not ready for use.
+#'
+#' @param ... A set of time unit objects (e.g., `tu_year(1)`, `tu_month(2)`, etc.)
+#'
+#' @return An object of class `mt_duration` representing the specified duration.
+#'
+#' @details
+#' This is a low-level constructor function that creates duration objects.
+#'
+#' @export
+#' @rdname duration
+new_duration <- function(...) {
+  vctrs::new_vctr(list2(...), class = "mt_duration")
+}
