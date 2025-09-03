2da84420b98046e0e4b059204f93b9a16d2b6fa9
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2025-09-03 17:07:22 +1000

    Add primitive cyclical time constructor

---
+++ R/cyclical_time.R
@@ -0,0 +1,55 @@
+#' Cyclical time representation
+#' 
+#' `cyclical_time()` creates a cyclical time representation using specified
+#' cycle and chronon (and optionally display granules). The cycles is the 
+#' larger time unit that defines the time period over which the chronon loops
+#' over (e.g. a week, `tu_week(1L)`). The chronon is the smaller time unit that
+#' iterates within each cycle (e.g. a day, `tu_day(1L)`). Combined, these two
+#' granules form a cyclical time point (e.g. day of the week).
+#' 
+#' @param chronon A time unit object representing the chronon (e.g., `tu_month(1L)`)
+#' @param cycle A time unit object representing the cycle (e.g., `tu_year(1L)`)
+#' @param granules An optional list of time unit objects used in the display (e.g., `list(tu_quarter(1L))`)
+#' 
+#' @return An function used to create cyclical time points.
+#' 
+#' @examples
+#' 
+#' day_of_week <- cyclical_time(tu_day(1L), tu_week(1L))
+#' day_of_week(Sys.Date())
+#' 
+#' month_of_year <- cyclical_time(tu_month(1L), tu_year(1L))
+#' month_of_year(Sys.Date())
+#' 
+cyclical_time <- function(chronon, cycle, granules = list()) {
+  if (!all(vapply(granules, function(g) inherits(g, "mixtime::mt_unit"), logical(1L)))) {
+    stop("All elements in granules must be time unit objects", call. = FALSE)
+  }
+  
+  if (!inherits(chronon, "mixtime::mt_unit")) {
+    stop("`chronon` must be a time unit object", call. = FALSE)
+  }
+  if (!inherits(cycle, "mixtime::mt_unit")) {
+    stop("`cycle` must be a time unit object", call. = FALSE)
+  }
+  
+  # TODO: Ensure c(granules, chronon) are in decreasing order of size
+
+  function(.data, tz = "UTC") {
+    # Cast to continuous time from Date, POSIXct, etc.
+    .data <- time_cast(.data, chronon)
+
+    # Reduce to cyclical time using modulo arithmetic
+    .data <- .data%%calendar_algebra(cycle, chronon)
+
+    if (!is.character(tz) || length(tz) != 1L) {
+      cli::cli_abort("{.tz} must be a length 1 string describing the timezone. Mixed timezones currently need to be combined separately.")
+    }
+
+    vctrs::new_vctr(
+      .data, 
+      class = c("mt_cyclical", "mt_time"),
+      tz = tz, granules = granules, chronon = chronon, cycle = cycle
+    )
+  }
+}
