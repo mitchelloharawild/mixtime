c5f8ae69f73465745f2ae8b6c3b8eafdfe2d5072
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2025-12-16 17:12:15 +1100

    Remove originless time tests

--- tests/testthat/test-yearmonth.R
+++ tests/testthat/test-yearmonth.R
@@ -1,6 +1,6 @@
 test_that("yearmonth() format", {
   ym <- yearmonth(0:11)
-  expect_equal(format(ym), paste("1970", month.abb))
-  ym <- ym - yearmonth(0)
-  expect_equal(format(ym), paste0(0:11, " month", ifelse(0:11!=1,"s","")))
+  expect_equal(format(ym), paste("1970", month.abb, sep = "-"))
+  # ym <- ym - yearmonth(0)
+  # expect_equal(format(ym), paste0(0:11, " month", ifelse(0:11!=1,"s","")))
 })
