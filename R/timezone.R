19ee148e9307fce28a4fe2911d98ffcfd23f57ab
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-02-10 17:50:43 +1100

    Chronon specific tz_offset() output

--- R/timezone.R
+++ R/timezone.R
@@ -10,27 +10,35 @@
 #' @examples
 #' tz_name(Sys.time())
 #' tz_name(as.POSIXct("2024-06-15 12:00:00", tz = "America/New_York"))
-tz_name <- S7::new_generic("tz", "x")
+tz_name <- S7::new_generic("tz_name", "x")
 S7::method(tz_name, S7::class_POSIXt) <- function(x) attr(x, "tzone") %||% "UTC"
 S7::method(tz_name, mt_tz_unit) <- function(x) x@tz
+S7::method(tz_name, S7::new_S3_class("mt_linear")) <- function(x) tz_name(time_chronon(x))
 S7::method(tz_name, S7::class_any) <- function(x) "UTC"
 
 #' Get timezone offset
 #'
 #' Returns the UTC offset for a given datetime in its specified timezone.
 #'
-#' @param x A POSIXct datetime object or something coercible to POSIXct.
-#'   The timezone is extracted from this object.
+#' @param x A time class coercible to POSIXt with an associated time zone.
 #'
-#' @return A numeric vector of offsets in seconds from UTC.
+#' @return A numeric vector of offsets from UTC in the same chronon (e.g. seconds for POSIXt, days for dates, etc.)
 #' @export
 #'
 #' @examples
 #' tz_offset(Sys.time())
 #' tz_offset(as.POSIXct("2024-06-15 12:00:00", tz = "America/New_York"))
-tz_offset <- function(x) {
-  x <- as.POSIXct(x)
-  get_tz_offset(x, tz_name(x))
+tz_offset <- S7::new_generic("tz_offset", "x")
+S7::method(tz_offset, S7::class_POSIXct) <- function(x) get_tz_offset(x, tz_name(x))
+method(tz_offset, S7::new_S3_class("mixtime")) <- function(x) {
+  vecvec::unvecvec(
+    vecvec::vecvec_apply(x, tz_offset)
+  )
+}
+method(tz_offset, S7::new_S3_class("mt_linear")) <- function(x, to, discrete = FALSE, ...) {
+  time_s <- as.POSIXct(x)
+  offset_s <- get_tz_offset(time_s, tz_name(x))
+  offset_s*chronon_cardinality(tu_second(1L), time_chronon(x), time_s)
 }
 
 #' Get timezone abbreviation
