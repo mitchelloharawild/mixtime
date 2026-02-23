bc8231e091dd879c8c7e1bea8d740a2b9a3a5487
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-02-23 16:59:04 +1100

    Add param docs for mt_loc_unit

--- R/cal0_time_solar.R
+++ R/cal0_time_solar.R
@@ -1,3 +1,15 @@
+# #' Location unit class with geographic coordinates
+# #'
+# #' A time unit class that extends mt_unit to include geographic location
+# #' information for solar-based time calculations.
+# #'
+#' @param lat Numeric. Latitude in decimal degrees. Range: -90 to 90.
+#'   Default: 0 (equator).
+#' @param lon Numeric. Longitude in decimal degrees. Range: -180 to 180.
+#'   Default: 0 (Prime Meridian).
+#' @param alt Numeric. Altitude in meters above sea level.
+#'   Default: 0 (sea level).
+#'
 #' @rdname mt_unit
 #' @export
 mt_loc_unit <- S7::new_class(
