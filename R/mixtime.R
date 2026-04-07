dedc760884c0714c66eb9b624057a57def1da3cd
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-04-07 19:43:01 +1000

    Document mixtime() Resolves #56

--- R/mixtime.R
+++ R/mixtime.R
@@ -48,6 +48,53 @@ new_time <- function(x, chronon = NULL, cycle = NULL) {
     cycle = cycle
   )
 }
+
+#' Create a mixtime vector
+#'
+#' A mixtime is a vector which describes a point in time. It uses a calendar
+#' definition to translate a vector of numbers into a point in time.
+#'
+#' @param data A vector of time values. This can be a character vector (e.g. "2024-01-01"),
+#'  a numeric vector (e.g. seconds since epoch), or a time class (e.g. Date, POSIXct, yearmonth, etc.).
+#' @param chronon A time unit object representing the smallest indivisible time unit (chronon) for the mixtime. This is used to interpret the numeric values in `data` and to define the time resolution of the mixtime. If not provided, it will be inferred from `data`.
+#' @param cycle An optional time unit object representing the cycle for cyclical time. This is used to define the repeating cycle for cyclical time representations (e.g. day-of-week, month-of-year). If not provided, the mixtime will be treated as linear time.
+#' @param discrete A logical indicating whether the time values should be treated as discrete (integer) or continuous (fractional). This affects how numeric values are interpreted and how time arithmetic is performed. The default is `TRUE` (discrete).
+#' 
+#' @return A `mixtime` object representing the time values in `data` according to the specified `chronon` and `cycle`.
+#' 
+#' @examples
+#' # Create a mixtime for today
+#' mixtime(Sys.Date())
+#' 
+#' # Create a mixtime for the current date and time
+#' mixtime(Sys.time())
+#' 
+#' # Create a mixtime for the time of day (cyclical time)
+#' mixtime(Sys.time(), cycle = cal_gregorian$day(1L))
+#' 
+#' # Specify a timezone for the chronon
+#' mixtime(Sys.time(), chronon = cal_gregorian$second(1L, tz = Sys.timezone()))
+#' mixtime(Sys.time(), chronon = cal_gregorian$second(1L, tz = "Pacific/Honolulu"))
+#' mixtime(Sys.time(), chronon = cal_gregorian$second(1L, tz = "Australia/Melbourne"))
+#' 
+#' # Dates (and all granularities) can have timezones
+#' mixtime(Sys.time(), chronon = cal_gregorian$day(1L, tz = Sys.timezone()))
+#' mixtime(Sys.time(), chronon = cal_gregorian$day(1L, tz = "Pacific/Honolulu"))
+#' mixtime(Sys.time(), chronon = cal_gregorian$day(1L, tz = "Australia/Melbourne"))
+#' 
+#' # Continuous time tracks progress within the chronon
+#' mixtime(Sys.time(), chronon = cal_gregorian$day(1L, tz = Sys.timezone()), discrete = FALSE)
+#' 
+#' # Mixtime can combine different granularities and timezones in a vector
+#' now <- Sys.time()
+#' c(
+#'   # Datetime (second chronon) in UTC
+#'   mixtime(now),
+#'   # Date (minute chronon) in local timezone
+#'   mixtime(now, chronon = cal_gregorian$minute(1L, tz = Sys.timezone())),
+#'   # Month (month chronon) in UTC
+#'   mixtime(now, chronon = cal_gregorian$month(1L))
+#' )
 #' @export
 mixtime <- function(data, chronon = time_chronon(data), cycle = time_cycle(data), discrete = TRUE) {
   # Add default tz if not given in chronon or cycle
