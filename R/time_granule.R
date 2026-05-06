f36c4557cdd90f9fe202709f7d2906d62ea27cab
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-05-06 14:23:02 +0200

    Only replace naive properties when inheriting from other granules

--- R/time_granule.R
+++ R/time_granule.R
@@ -1,8 +1,8 @@
 #' @importFrom S7 props props<-
 granule_inherit_props <- function(x, parent) {
-  waiver_props <- is.naive(props <- props(x))
+  naive_props <- is.naive(props <- props(x))
   parent_props <- props(parent)
-  inherit_props <- intersect(names(waiver_props), names(parent_props))
+  inherit_props <- intersect(names(props)[naive_props], names(parent_props))
   props[inherit_props] <- parent_props[inherit_props]
   props(x) <- props
   x
