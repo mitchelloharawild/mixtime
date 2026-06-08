b261e626aa533e6a8b6f67b214f30309195ffa6c
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-06-08 13:37:50 +0200

    Skip cycle overflow check for sequences with unknown cardinality

--- R/sequence.R
+++ R/sequence.R
@@ -176,7 +176,7 @@ seq.mt_time <- function(
     on_invalid <- match.arg(on_invalid)
     cycle_size <- chronon_cardinality(chronon, by, res)
     # Check if the sequence offset will overflow the cycle
-    if (any(if (by@n < 0) cycle_size >= seq_part else cycle_size <= seq_part)) {
+    if (any(if (by@n < 0) cycle_size >= seq_part else cycle_size <= seq_part, na.rm = TRUE)) {
       if (missing_on_invalid) {
         cli::cli_warn(c(
           "The cycle offset ({seq_part + 1L} {time_unit_full(chronon)}{cli::qty(seq_part)}{?s}) has produced time points that overflow the {time_unit_full(by)} cycle.",
